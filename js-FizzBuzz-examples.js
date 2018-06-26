0dec69d35519ced4e46e6492652041ceb32e8b30
Jamie Wilson <jamie@jamiewilson.io>
2018-06-26 09:58:49 -0500

    improving  function, cleaning up

--- js-FizzBuzz-examples.js
+++ js-FizzBuzz-examples.js
@@ -4,13 +4,13 @@
 
 // for numbers 1—100
 for (i = 1; i <= 100; i++) {
-	// if it's divisible by both 3 & 5, print 'FizzBuzz'
+  // if it's divisible by both 3 & 5, print 'FizzBuzz'
   if (i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz')
-	// if it's divisible by 3, print 'Fizz'
+  // if it's divisible by 3, print 'Fizz'
   else if (i % 3 === 0) console.log('Fizz')
-	// if it's divisible by 5, print 'Buzz'
+  // if it's divisible by 5, print 'Buzz'
   else if (i % 5 === 0) console.log('Buzz')
- 	// otherwise, print the current number
+  // otherwise, print the current number
   else console.log(i)
 }
 
--- js-html-template-imports/import-templates.js
+++ js-html-template-imports/import-templates.js
@@ -1,10 +1,10 @@
 const $ = (selector, target = document) => target.querySelector(selector)
-const $$ = selector => document.querySelectorAll(selector)
+const $all = (selector) => document.querySelectorAll(selector)
 const $on = (type, target, callback) => target.addEventListener(type, callback)
 
 $on('load', window, () => {
-	$$('link[rel=import]').forEach(href => {
-		document.head.appendChild($('style', href.import))
-		document.body.appendChild($('template', href.import).content)
-	})
+  $all('link[rel=import]').forEach(href => {
+    document.head.appendChild($('style', href.import))
+    document.body.appendChild($('template', href.import).content)
+  })
 })
